10d33269b6dbc1c9a7636df8f7fd0982e0e5253c
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2025-03-25 21:17:17 +0100

    Deploy the sprinng Boot app and config the DB

---
+++ backEnd/hairdressin/src/main/java/com/api/hairdressin/controllers/TestControler.java
@@ -0,0 +1,17 @@
+package com.api.hairdressin.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class TestControler {
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return new String("Hi, Sprinng Boot");
+    }
+    
+
+
+}
---
+++ backEnd/hairdressin/src/test/java/com/api/hairdressin/HairdressinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hairdressin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairdressinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
