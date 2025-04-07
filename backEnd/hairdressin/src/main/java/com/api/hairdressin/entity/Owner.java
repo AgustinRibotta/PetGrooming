71d9113812f39eabf7737c34e18dd35d014b1c87
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2025-04-07 20:58:31 +0200

    Generate entity Pet and Owner

--- backEnd/hairdressin/src/main/java/com/api/hairdressin/entity/Owner.java
+++ backEnd/hairdressin/src/main/java/com/api/hairdressin/entity/Owner.java
@@ -1,11 +1,14 @@
 package com.api.hairdressin.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,20 +19,17 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pet {
+public class Owner {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
+
     private String name;
-    private String race;
-    private String color;
-    private String allergic;
-    private String special_attention;
-    private String observations;
 
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
-    private Owner oneOwner;
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
+    @OneToMany(mappedBy = "oneOwner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Pet> pets;
 }
