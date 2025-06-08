0709fb550fbe3521337d4ede28f3f5e3812c31b7
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2025-06-08 18:44:00 +0200

    Test final kubernetes config

--- frontend/src/api/axiosClients.ts
+++ frontend/src/api/axiosClients.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://127.0.0.1:37275/api",
+    baseURL: "http://127.0.0.1:30415/api",
     headers: {
         "Content-Type": "application/json",
     },
